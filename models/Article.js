864d2004afc94cac5f5400fd1c6a3baf03742d0e
John Young <87405768+jyoung0613@users.noreply.github.com>
2021-10-25 13:13:47 -0600

    added models and main handlebars file

---
+++ models/Article.js
@@ -0,0 +1,49 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Article extends Model {}
+
+Article.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        content: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+        },
+        created_at: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW,
+        },
+        updated_at: {
+            type: DataTypes.DATE,
+            defaultValue: "",
+            onUpdate: DataTypes.NOW,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id',
+            },
+        }, 
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        // timestamps: true,
+        underscored: true,
+        modelName: 'Article',
+    }
+);
+
+module.exports = Article;
